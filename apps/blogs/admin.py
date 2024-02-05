62c058843824e976b58dc7a939555684e6997c27
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-02-05 13:19:23 +0600

    [#9] add models blog and translation settings

--- apps/blogs/admin.py
+++ apps/blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 
-# Register your models here.
+from apps.blogs.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(TranslationAdmin):
+    list_display = ("id", "title")
+    list_display_links = ("id",)
--- x_dev/urls.py
+++ x_dev/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +7,6 @@ urlpatterns = [
 
     path("api/users/", include("apps.users.urls")),
     path("api/", include("apps.teams.urls")),
+
+    path("i18n/", include("django.conf.urls.i18n")),
 ]
