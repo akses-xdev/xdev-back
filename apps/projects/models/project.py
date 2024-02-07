13b454b4ee93aea0b1e876b73ffb76ef8401dc7a
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-02-07 23:19:01 +0600

    fix migrations and add applications app

--- apps/projects/models/project.py
+++ apps/projects/models/project.py
@@ -5,9 +5,8 @@ from apps.projects.models import Category
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
-    preview = models.URLField(blank=True, null=True)
+    preview = models.ImageField(upload_to='projects_poster/', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    poster = models.ImageField(upload_to='projects_poster/', blank=True, null=True)
 
     def __str__(self):
         return self.title
