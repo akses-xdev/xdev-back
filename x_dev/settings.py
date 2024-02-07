10dff021f7745c5c5b3492779d829b44a157cee9
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-02-07 23:43:08 +0600

    settings corsheaders

--- x_dev/settings.py
+++ x_dev/settings.py
@@ -13,6 +13,8 @@ SECRET_KEY = 'django-insecure-04ged$o(h_bi9ebi11wpi@*^y=ys^nwzzv1+k&whv&hs2b80*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'users.User'
@@ -31,6 +33,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 
     'apps.users.apps.UsersConfig',
     'apps.teams.apps.TeamsConfig',
@@ -137,3 +140,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# corsheaders
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
