dbc0b70978204b1fac0d63fbb0628a027de72441
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-02-08 22:55:04 +0600

    feat search

--- apps/blogs/views/blogs/list.py
+++ apps/blogs/views/blogs/list.py
@@ -1,4 +1,6 @@
+from django.db.models import Q
 from rest_framework import permissions, generics
+from django.utils.translation import get_language
 
 from apps.blogs.models import Blog
 from apps.blogs.serializers import BlogSerializer
@@ -6,5 +8,19 @@ from apps.blogs.serializers import BlogSerializer
 
 class ListBlogAPIView(generics.ListAPIView):
     permissions = [permissions.AllowAny]
-    queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+
+    def get_queryset(self):
+        language = get_language()
+        search_query = self.request.query_params.get('search', '')
+
+        if search_query:
+            queryset = Blog.objects.filter(
+                Q(**{f'title_{language}__icontains': search_query}) |
+                Q(**{f'description_{language}__icontains': search_query}) |
+                Q(**{f'content_{language}__icontains': search_query})
+            )
+        else:
+            queryset = Blog.objects.all()
+
+        return queryset
