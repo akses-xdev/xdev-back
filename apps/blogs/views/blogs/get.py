dff7e52c8a138f6de2d58ca99f0a4d038748f0e1
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-02-08 22:59:40 +0600

    fix views blog

--- apps/blogs/views/blogs/get.py
+++ apps/blogs/views/blogs/get.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from rest_framework.response import Response
 
 from apps.blogs.models import Blog
 from apps.blogs.serializers import BlogSerializer
@@ -9,3 +10,13 @@ class GetBlogAPIView(generics.RetrieveAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     lookup_field = "id"
+
+    def increment_views(self, blog):
+        blog.views += 1
+        blog.save()
+
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.increment_views(instance)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
